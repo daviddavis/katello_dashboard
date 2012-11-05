baa80aa5e547f7fe657d44c06428cd13def747a7
David Davis <daviddavis@redhat.com>
2012-11-05 18:12:09 -0500

    Some UI tweaks

--- jobs/iteration.rb
+++ jobs/iteration.rb
@@ -7,6 +7,6 @@ DEADLINE = "November 15th, 2012 12:00 AM"
 SCHEDULER.every '1s' do
   time_left = (Chronic.parse("#{DEADLINE}") - Time.now).to_i
   text = "#{ChronicDuration.output(time_left)} left"
-  title = "Iteration #{CURRENT_ITERATION}"
+  title = "Iteration ##{CURRENT_ITERATION}"
   send_event('iteration', { title: title, text: text })
 end
